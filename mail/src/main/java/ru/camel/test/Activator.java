68ebbaf770b914fe0f81835c432ec3ee19dff9b2
Roman S.A <raskidan@ya.ru>
2017-11-05 19:30:07 +0300

    mail module

---
+++ mail/src/main/java/ru/camel/test/Activator.java
@@ -0,0 +1,30 @@
+package ru.camel.test;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.core.osgi.OsgiDefaultCamelContext;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+    private CamelContext camelContext;
+
+    public void start(BundleContext bundleContext) throws Exception {
+        System.out.println("::::START MAIL BUNDLE::::");
+        camelContext = new OsgiDefaultCamelContext(bundleContext);
+        camelContext.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("imaps://imap.yandex.ru?username=raskidan&password=2886890553roma$&delete=false&unseen=true&consumer.delay=1")
+                        .process(new LoggerClass());
+
+            }
+        });
+        camelContext.start();
+    }
+
+    public void stop(BundleContext bundleContext) throws Exception {
+        camelContext.stop();
+        System.out.println("::::STOP MAIL BUNDLE::::");
+    }
+}
---
+++ mail/src/main/java/ru/camel/test/LoggerClass.java
@@ -0,0 +1,12 @@
+package ru.camel.test;
+
+import org.apache.camel.Exchange;
+
+public class LoggerClass implements org.apache.camel.Processor {
+
+    public void process(Exchange exchange) throws Exception {
+        System.out.println("\nSubject: " + exchange.getIn().getHeaders());
+        String body = exchange.getIn().getBody(String.class);
+        System.out.println("\nBody: " + body);
+    }
+}
